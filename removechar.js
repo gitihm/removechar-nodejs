189bc674098e26d144ca5470f067deb79b60966d
gitihm <ismael897@outlook.com>
2019-01-27 01:52:52 +0700

    edit file to JSON

--- removechar.js
+++ removechar.js
@@ -2,16 +2,22 @@ var str = "ab1234bc"
 var ch = "b"
 var newstr = ""
 
-remove = () => {
-    let i=0,j=0
-    for(key in str){
-        if(str[key]!=ch){
-            newstr =newstr+str.substr(key--,1)
-            console.log(str[key]) 
-    }
-    console.log(str)
-    console.log(newstr)
-    }
+var str1 ={ 
+    str : "abcdbe",
+    ch : 'b',
+    newstr :''
 }
 
+remove = () => {
+    let i=0,j=0
+    for(key in str1.str){
+        if(str1.str[key]!=str1.ch){
+            str1.newstr =str1.newstr+str1.str.substr(key--,1)
+            //console.log(str1.str[key]) 
+        }
+    }
+    console.log(str1.str)
+    console.log(str1.newstr)
+    
+}
 remove()
